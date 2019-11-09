e7eef42fa21276405b56089c379a2f4726ee9f03
Rasori <Rasori@users.noreply.github.com>
2019-11-09 22:50:36 +0100

    Update RCP_conv.py

--- python/RCP_conv.py
+++ python/RCP_conv.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python
+# Modified by Waltteri Koskinen for Tampere Formula Student
+# 09.11.2019
+# Changes:
+#		V1.6:
+#			Minor bug fixes
+#			Modifications for .exe conversion for users with limited IT-skills
+
+
 #  Author:      Daniel Poulter  mrpilt@gmail.com
 #  Script Name:   RCP_conv.py
 #  Function:      Takes Race Capture Pro csv logs and converts them to a
@@ -6,7 +14,7 @@
 #         Dlog99 into .srt files which can be opened by GEMS Data Analysis
 #         or AEM Data Analysis.
 #  Notes:   Thanks to John Freund for working out required format for GEMS in rccsv2gems.pl
-# 			Max Value in GEMS CSV inport file is 2147483
+# 			Max Value in GEMS CSV import file is 2147483
 #  Version:    1.0
 #
 # Changes:
@@ -20,21 +28,19 @@ import csv
 import sys
 
 
-
-
-
-def RCP_to_GEMS(filename, num_of_sats=0, output_filename = None):
+def RCP_to_GEMS(filename, num_of_sats=0, output_filename=None):
 	to_rads = 0.01745329251
 	with open(filename, 'rb') as input_file:
 		if not output_filename:
 			output_filename = filename.split('.')[0] + '_GEMS.csv'
+			print output_filename
 		with open(output_filename, 'wb') as output_file:
-			reader = csv.reader(input_file, quoting=csv.QUOTE_NONNUMERIC)     #Reads all input as floats
+			reader = csv.reader(input_file, quoting=csv.QUOTE_NONNUMERIC)     # Reads all input as floats
 			writer = csv.writer(output_file)
 
-			#Example headers: Interval,Utc,Battery,AccelX,AccelY,AccelZ,Yaw,Pitch,Roll,Latitude,Longitude,Speed,Distance,GPSSats,LapCount,LapTime,Sector,SectorTime,PredTime
-		 	header = []
-		 	title_row = reader.next()
+			# Example headers: Interval,Utc,Battery,AccelX,AccelY,AccelZ,Yaw,Pitch,Roll,Latitude,Longitude,Speed,Distance,GPSSats,LapCount,LapTime,Sector,SectorTime,PredTime
+			header = []
+			title_row = reader.next()
 			for column in title_row:
 				header.append(column.split("|")[0])
 
@@ -42,6 +48,7 @@ def RCP_to_GEMS(filename, num_of_sats=0, output_filename = None):
 				GPSSats_index = header.index('GPSSats')
 				Latitude_index = header.index('Latitude')
 				Longitude_index = header.index('Longitude')
+
 			except:
 				GPSSats_index = 0
 				Latitude_index = 0
@@ -49,67 +56,78 @@ def RCP_to_GEMS(filename, num_of_sats=0, output_filename = None):
 
 			writer.writerows([header])
 
-			#Skip rows until a GPS fix occurs
+			# Skip rows until a GPS fix occurs
 			if GPSSats_index != 0:
 				skipped = 0
+
 				while True:
 					try:
-						if reader.next()[GPSSats_index] >= num_of_sats:
-							print "%d lines skipped"%skipped
+						row = reader.next()
+						if row[GPSSats_index] == '':
+							row[GPSSats_index] = 0
+						if row[GPSSats_index] >= num_of_sats:
+							print "%d lines skipped" % skipped
 							break
 						skipped += 1
-					except ValueError:		#Illegal Character :- Continue on next line
-						print "Warning: Illegal Character on row %d "%reader.line_num
+					except ValueError:		# Illegal Character :- Continue on next line
+						print "Warning: Illegal Character on row %d " % reader.line_num
 						continue
-					except Exception, e:	#All other exceptions
-						print "Read Error : %s"%e
-						return (False, e)
+					except IndexError, e:
+						print "Read Error : %s" % e
+						print "Try to lower minimum satellite value"
+						raw_input("Press Enter to continue...")
+						return False, e
+					except Exception, e:  # All other exceptions
+						print "Read Error : %s" % e
+						raw_input("Press Enter to continue...")
+						return False, e
 
-
-			#First row is handled differently, from here we get the start time and populate all blanks with 0.0
-			first_row  = reader.next()
+			# First row is handled differently, from here we get the start time and populate all blanks with 0.0
+			first_row = reader.next()
 			start_time = int(first_row[0])
 			start_time_gps = int(first_row[1])
+			first_row[0] = 0.0 															# Set time to zero
+			first_row[1] = 0.0 															# Set GPS time to
 
-			for i in range(len(first_row)):
-				if first_row[i] == '':
-					first_row[i] = 0.0
-
-			first_row[0] = 0.0 															#Set time to zero
-			first_row[1] = 0.0 															#Set GPS time to
 			if GPSSats_index != 0:
-				first_row[Longitude_index] = first_row[Longitude_index] * to_rads			#Convert to radians
-				first_row[Latitude_index] = first_row[Latitude_index] * to_rads				#Convert to radians
-			writer.writerows([first_row])												#Write to output
+				first_row[Longitude_index] = first_row[Longitude_index] * to_rads			# Convert to radians
+				first_row[Latitude_index] = first_row[Latitude_index] * to_rads				# Convert to radians
+			writer.writerows([first_row])												# Write to output
 			previous_row = first_row
 
-			while True: #Runs until exception occurs
+			while True: # Runs until exception occurs
 
 				try:
 					current_row  = reader.next()
-				except StopIteration:	#Reached end of file
-					return (True, output_filename)
-				except ValueError:		#Illegal Character :- Continue on next line
-					print "Warning: Illegal Character on row %d "%reader.line_num
+				except StopIteration:  # Reached end of file
+					return True, output_filename
+				except ValueError:		# Illegal Character :- Continue on next line
+					print "Warning: Illegal Character on row %d " % reader.line_num
 					continue
-				except Exception, e:	#All other exceptions
-					print "Read Error : %s"%e
-					return (False, e)
+				except Exception, e:  # All other exceptions
+					print "Read Error : %s"% e
+					raw_input("Press Enter to continue...")
+					return False, e
 
-				current_row[0] = (int(current_row[0]) - start_time)/1000.0  				#Convert time
-				current_row[1] = (int(current_row[1]) - start_time_gps)/1000.0  			#Convert time
-				if GPSSats_index != 0:
-					current_row[Longitude_index] = current_row[Longitude_index] * to_rads		#Convert to radians
-					current_row[Latitude_index] = current_row[Latitude_index] * to_rads			#Convert to radians
-
-				#If blanks found, populate with last known good value
 				for i in range(len(current_row)):
+					try:
+						if i == Longitude_index or i == Latitude_index:
+							current_row[i] = current_row[i] * to_rads
+					except TypeError:
+						if current_row[i] == '':
+							current_row[i] = previous_row[i]
+
+					# If blanks found, populate with last known good value
 					if current_row[i] == '':
 						current_row[i] = previous_row[i]
 
+				# print current_row[Battery_index]
+
+				current_row[0] = (int(current_row[0]) - start_time)/1000.0  				#Convert time
+				current_row[1] = (int(current_row[1]) - start_time_gps)/1000.0  			#Convert time
+
 				previous_row = current_row
 				writer.writerows([current_row])
-
 
 
 if __name__ == "__main__":
